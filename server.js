b96a241a4aaa3a613ea468dd75e6503c1cb08184
Ailton Vieira Pinto Filho <ailtonvivaz@outlook.com>
2020-04-18 14:39:08 -0300

    Implement MongoDB with mongoose

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 var express = require('express');
+const mongoose = require('mongoose');
+
 var app = express();
+mongoose.Promise = Promise
 
 const PORT = process.env.PORT || 5000;
+const DB_URI = 'mongodb://mongo:27017/reflection-database';
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-app.listen(PORT, function () {
-    console.log(`Example app listening on port ${PORT}!`);
-});
+const run = async () => {
+    await mongoose.connect(DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+    app.listen(PORT, function () {
+        console.log(`Example app listening on port ${PORT}!`);
+    });
+}
+
+run();
+
